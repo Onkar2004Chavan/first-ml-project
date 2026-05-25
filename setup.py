ae01109fde5abd4530b7441de105f69f8117a628
Onkar2004Chavan <chavan2004onkar@gmail.com>
2026-05-25 11:28:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+    return requirements
+
+setup(
+    name='ML Project',
+    version='0.0.1',
+    author='Onkar',
+    author_email='chavan2004onkar@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
